2f5b27b133b599d7f8cff4f155b909df723b4679
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2016-07-25 15:34:23 -0700

    Migrate Token

--- db/migrate/20160725153011_create_tokens.rb
+++ db/migrate/20160725153011_create_tokens.rb
@@ -1,4 +1,11 @@
 class CreateTokens < ActiveRecord::Migration
   def change
+    create_table :tokens do |t|
+      t.string :code
+      t.integer :generator_id
+      t.integer :new_member_id
+
+      t.timestamps null: false
+    end
   end
 end
