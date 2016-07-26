855046f117936dfcfc38e7a75591f8b2ea3a0b2e
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2016-07-26 10:16:45 -0700

    Begin ajax function

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,23 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  console.log('ready')
+  showListingForm();
 });
+
+function showListingForm(){
+  $('a.btn.btn-info').on('click', function(event){
+    event.preventDefault();
+
+    $.ajax({
+      url: '/listings/new',
+      method: 'GET'
+    })
+    .done(function(form){
+      console.log('good job homie')
+      console.log(form)
+    })
+    .fail(function(){
+      console.log("Keep trying")
+    })
+  })
+
+}
