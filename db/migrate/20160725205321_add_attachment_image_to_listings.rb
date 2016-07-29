e03dbf4d5e747120f132bd5d4d3b420ed98356e3
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2016-07-28 17:17:53 -0700

    Update message migration

--- db/migrate/20160725205321_add_attachment_image_to_listings.rb
+++
@@ -1,9 +0,0 @@
-class AddAttachmentImageToListings < ActiveRecord::Migration
-    def up
-      add_attachment :listings, :image
-    end
-
-    def down
-      remove_attachment :listings, :image
-    end
-end
